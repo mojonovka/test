a9e28feab00d08bfabf089c6f5fcc276321265a3
mojonovka <69107672+mojonovka@users.noreply.github.com>
2020-10-15 21:19:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 <head>
 </head>
 <body>
-<h1>test main page</h1>
-<?php
-echo "php WORK!!!";
-?>
+  <h1>test main page</h1>
+  <?php echo "php WORK!!!"; ?>
   <p>Agregadi directamente en GitHub</p>
 </body>
 </html>
