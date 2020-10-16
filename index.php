51d625d0bef1470fafa04f9fe569b0d42a561ffc
rv <mojonovka@gmail.com>
2020-10-16 09:07:21 +0200

    add javascript

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
 <head>
+	<script src="js/main.js"></script>
 </head>
 <body>
   <h1>test main page</h1>
   <?php echo "php WORK!!!"; ?>
   <p>Agregadi directamente en GitHub</p>
+
+<input type="button" onClick="saludo()" value="SALUDAR">
+
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// JavaScript
+
+function saludo(){
+	alert("Hola alumnos");
+}
